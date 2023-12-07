9e00af576a03193c35a24fb1a67ada96b740bb8b
hieu <hieu.van@goatstudio.tech>
2023-12-07 13:59:20 +0700

    feat: students CRUD added, responses separated, removed the use of HTTPException

---
+++ basemodels/students.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class CreateStudentsDeserializer(BaseModel):
+    name: str
+    age: int
+    course: str
+
+
+class GetStudentsDeserializer(BaseModel):
+    id: Optional[int] = None
+    name: Optional[str] = None
+    age: Optional[int] = None
+    course: Optional[str] = None
+    is_deleted: Optional[bool] = None
+
+
+class UpdateStudentsDeserializer(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+    course: Optional[str] = None
+    is_deleted: Optional[bool] = None
--- main.py
+++ main.py
@@ -1,24 +1,20 @@
-from fastapi import FastAPI, HTTPException, Depends, status
-from pydantic import BaseModel
+from fastapi import FastAPI, Depends, status
 from typing import Optional, Annotated
-
-import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
+
+from basemodels import GetStudentsDeserializer, UpdateStudentsDeserializer, CreateStudentsDeserializer
+import models
+from common.responses import APIResponseMessage
+
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
 
-class StudentBase(BaseModel):
-    name: str
-    age: int
-    course: str
-
-
-
 def get_db():
     db = SessionLocal()
+
     try:
         yield db
     finally:
@@ -29,53 +25,77 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 
 @app.post("/students/create_student/", status_code=status.HTTP_201_CREATED)
-async def create_student(student: StudentBase, db: db_dependency):
-    db_student = models.Student(**student.model_dump())
-    db.add(db_student)
-    db.commit()
+async def create_student(student: CreateStudentsDeserializer, db: db_dependency):
+    try:
+        db_student = models.Student(**student.model_dump())
+        db.add(db_student)
+        db.commit()
+        db.refresh(db_student)
 
-    response = {
-        "status_code": status.HTTP_201_CREATED,
-        "message": "Student created successfully",
-        "data": db_student
-    }
+        APIResponseMessage.SUCCESSFULLY_CREATED["data"] = db_student
 
-    return response
+        return APIResponseMessage.SUCCESSFULLY_CREATED
+    except Exception as e:
+        return APIResponseMessage.FAILED_TO_CREATE
 
 
-@app.get("/students/get_student/", status_code=status.HTTP_200_OK)
-async def get_student(
-        id: Optional[int] = None,
-        name: Optional[str] = None,
-        age: Optional[int] = None,
-        course: Optional[str] = None,
-        is_deleted: Optional[bool] = None,
-        db: Session = Depends(get_db)
-):
+@app.post("/students/get_students/", status_code=status.HTTP_200_OK)
+async def get_student(students: Optional[GetStudentsDeserializer] = None, db: Session = Depends(get_db)):
     filters = []
 
-    if id is not None:
-        filters.append(models.Student.id == id)
-    if name is not None:
-        filters.append(models.Student.name == name)
-    if age is not None:
-        filters.append(models.Student.age == age)
-    if course is not None:
-        filters.append(models.Student.course == course)
-    if is_deleted is not None:
-        filters.append(models.Student.is_deleted == is_deleted)
+    if students:
+        if students.id is not None:
+            filters.append(models.Student.id == students.id)
+
+        if students.name is not None:
+            filters.append(models.Student.name == students.name)
+
+        if students.age is not None:
+            filters.append(models.Student.age == students.age)
+
+        if students.course is not None:
+            filters.append(models.Student.course == students.course)
+
+        if students.is_deleted is not None:
+            filters.append(models.Student.is_deleted == students.is_deleted)
 
     db_students = db.query(models.Student).filter(*filters).all()
 
     if not db_students:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No student found")
+        return APIResponseMessage.FAILED_TO_RETRIEVE
 
-    response = {
-        "status_code": status.HTTP_200_OK,
-        "message": "Students retrieved successfully",
-        "data": db_students
-    }
+    APIResponseMessage.SUCCESSFULLY_RETRIEVED["data"] = db_students
 
-    return response
+    return APIResponseMessage.SUCCESSFULLY_RETRIEVED
 
 
+@app.post("/students/update_student/", status_code=status.HTTP_200_OK)
+async def update_student(id: int, student: UpdateStudentsDeserializer, db: db_dependency):
+    db_student = db.query(models.Student).filter(models.Student.id == id).first()
+
+    if db_student:
+        if student.name is not None:
+            db_student.name = student.name
+        if student.age is not None:
+            db_student.age = student.age
+        if student.course is not None:
+            db_student.course = student.course
+
+        db.commit()
+        db.refresh(db_student)
+
+        APIResponseMessage.SUCCESSFULLY_UPDATED["data"] = db_student
+
+        return APIResponseMessage.SUCCESSFULLY_UPDATED
+    else:
+        return APIResponseMessage.FAILED_TO_RETRIEVE
+
+
+@app.post("/students/delete_student/", status_code=status.HTTP_200_OK)
+async def delete_student(id: int, db: db_dependency):
+    db_student = db.query(models.Student).filter(models.Student.id == id).first()
+
+    db_student.is_deleted = True
+    db.commit()
+
+    return APIResponseMessage.SUCCESSFULLY_DELETED
