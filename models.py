86e328820b049d637d07ab378024dbd299f69ec1
hieu <hieu.van@goatstudio.tech>
2023-12-07 10:24:31 +0700

    feat: student_id now increments by 1

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from database import Base
 class Student(Base):
     __tablename__ = 'students'
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
     name = Column(String(50), unique=True)
     age = Column(Integer, unique=False)
     course = Column(String(50), unique=False)
