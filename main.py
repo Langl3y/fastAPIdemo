f53b63861f54b6e0914c2d72ee25fc6a16307b9a
hieu <hieu.van@goatstudio.tech>
2023-12-07 17:22:48 +0700

    feat: sqlalchemy's automatic tables creating script added in main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from routers import students
+from models import Base
+from database import engine
 
 app = FastAPI()
+Base.metadata.create_all(bind=engine)
 
 app.include_router(students.router, prefix="/students")
 
