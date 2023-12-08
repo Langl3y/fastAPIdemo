b96dbadb853318b15738e658b9febacd390d2c82
hieu <hieu.van@goatstudio.tech>
2023-12-08 12:08:22 +0700

    hotfix: responses changed, db_student(s) variables are no longer static

--- common/responses.py
+++ common/responses.py
@@ -1,39 +1,28 @@
 from fastapi import status
 
 
-class APIResponseMessage:
-    SUCCESSFULLY_CREATED = {
-        "status_code": status.HTTP_201_CREATED,
-        "message": "created successfully",
-        "data": None
-    }
+class APIResponseCode(object):
+    FAILURE = (-1, 'General failure')  # General logic error
+    SUCCESS = (0, 'Success')  # Successful response
+    SERVER_ERROR = (1, 'Server error')  # Unexpected error during handling the request
+    BAD_REQUEST = (2, 'Bad request')  # Error returned by DRF serializer
+    NO_PERMISSION = (3, 'No permission')  # Error related to permissions
+    NOT_FOUND = (4, 'Not found')  # Object not found
+    ALREADY_EXISTS = (5, 'Already exists')  # Object already exists
+    VALIDATION_ERROR = (6, 'Validation error')  # Error related to invalidated input
+    INVALID_ACTION = (7, 'Invalid request')  # Invalid action (stateful)
+    ACTION_DENIED = (8, 'Action denied')  # Invalid action (stateless)
+    FILE_ERROR = (9, 'File error')  # Error related to file handling
+    DB_ERROR = (10, 'Database error')  # Error related to database
+    EXT_API_ERROR = (11, 'External API error')  # Error related to calling external API
+    TIMEOUT = (12, 'Timeout')  # Timeout when handling a request
+    EXPIRED = (13, 'Request expired')
+    TOO_MANY_REQUEST = (14, 'Too many request')
 
-    SUCCESSFULLY_RETRIEVED = {
-        "status_code": status.HTTP_200_OK,
-        "message": "retrieved successfully",
-        "data": None
-    }
+    @classmethod
+    def is_success(cls, code):
+        return code == cls.SUCCESS
 
-    SUCCESSFULLY_UPDATED = {
-        "status_code": status.HTTP_200_OK,
-        "message": "updated successfully",
-        "data": None
-    }
-
-    SUCCESSFULLY_DELETED = {
-        "status_code": status.HTTP_200_OK,
-        "message": "deleted successfully",
-    }
-
-    FAILED_TO_CREATE = {
-        "status_code": status.HTTP_409_CONFLICT,
-        "message": "could not be created",
-    }
-
-    FAILED_TO_RETRIEVE = {
-        "status_code": status.HTTP_204_NO_CONTENT,
-        "message": "Not found",
-    }
-
-
-
+    @classmethod
+    def is_failure(cls, code):
+        return code != cls.SUCCESS
